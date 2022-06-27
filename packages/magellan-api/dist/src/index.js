715f34941519e112d1b56c0b59a73f22edafdf6e
Josh Carver <jcarver989@gmail.com>
2022-06-26 23:01:08 -0700

    chore: Boilerplate for minimum bootable Apollo graphQL server

---
+++ packages/magellan-api/dist/src/index.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const apollo_server_1 = require("apollo-server");
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = (0, apollo_server_1.gql) `
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+  }
+`;
+const books = [
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin'
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster'
+    }
+];
+const resolvers = {
+    Query: {
+        books: () => books
+    }
+};
+const server = new apollo_server_1.ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: 'bounded'
+});
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
