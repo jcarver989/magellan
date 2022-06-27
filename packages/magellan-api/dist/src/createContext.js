3a77e73e2b0b138254de111b8380500a7c6e98ea
Josh Carver <jcarver989@gmail.com>
2022-06-26 23:01:08 -0700

    remove dist/ from source control

--- packages/magellan-api/dist/src/createContext.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createContext = void 0;
-const metastore_1 = require("./metastore");
-function createContext() {
-    return { metastoreClient: (0, metastore_1.getMetastoreClientFromEnv)() };
-}
-exports.createContext = createContext;
--- packages/magellan-api/dist/src/metastore/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getMetastoreClientFromEnv = exports.AwsGlueClient = void 0;
-var AwsGlueClient_1 = require("./AwsGlueClient");
-Object.defineProperty(exports, "AwsGlueClient", { enumerable: true, get: function () { return AwsGlueClient_1.AwsGlueClient; } });
-var getMetastoreClientFromEnv_1 = require("./getMetastoreClientFromEnv");
-Object.defineProperty(exports, "getMetastoreClientFromEnv", { enumerable: true, get: function () { return getMetastoreClientFromEnv_1.getMetastoreClientFromEnv; } });
